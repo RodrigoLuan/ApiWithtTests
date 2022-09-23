75c88bade5f5f1b467bdda2d4c79a72f6faeb1f1
Rodrigo Luan <rodrigo.luan@tige.com.br>
2022-09-22 21:02:53 -0300

    [Task-11] - findAll test implementated for the class userResource

--- src/test/java/br/com/devdicas/api/resources/UserResourceTest.java
+++ src/test/java/br/com/devdicas/api/resources/UserResourceTest.java
@@ -10,8 +10,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -64,7 +67,23 @@ class UserResourceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO() {
+        when(userService.findAll()).thenReturn(List.of(user));
+        when(modelMapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = userResource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(userDTO, response.getBody().get(INDEX));
+
+        assertEquals(userDTO.getId(), response.getBody().get(INDEX).getId());
+        assertEquals(userDTO.getName(), response.getBody().get(INDEX).getName());
+        assertEquals(userDTO.getEmail(), response.getBody().get(INDEX).getEmail());
+        assertEquals(userDTO.getPassword(), response.getBody().get(INDEX).getPassword());
     }
 
     @Test
